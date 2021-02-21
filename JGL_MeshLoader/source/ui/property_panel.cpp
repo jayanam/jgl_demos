b1828eff1ce62c950944be0050a04e0d5b73ad87
jayanam <jayanam.games@gmail.com>
2021-02-21 14:27:17 +0100

    Scene with light edits

--- JGL_MeshLoader/source/ui/property_panel.cpp
+++ JGL_MeshLoader/source/ui/property_panel.cpp
@@ -2,17 +2,22 @@
 #include "property_panel.h"
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "utils/imgui_widgets.h"
 
 namespace nui
 {
   void Property_Panel::render(nui::SceneView* scene_view)
   {
     ImGui::Begin("Properties");
-    ImGui::Text("Light:");
-
-    ImGui::ColorPicker3("Color", (float*)&scene_view->get_light()->mColor, ImGuiColorEditFlags_PickerHueWheel);
+    if (ImGui::CollapsingHeader("Light"))
+    {
+      ImGui::ColorPicker3("Color", (float*)&scene_view->get_light()->mColor, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_DisplayRGB);
+    
+      ImGui::Separator();
+      ImGui::Text("Position");
+      ImGui::Separator();
+      nimgui::draw_vec3_widget("Position", scene_view->get_light()->mPosition, 80.0f);
+    }
 
     ImGui::End();
   }
---
+++ JGL_MeshLoader/source/utils/imgui_widgets.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "pch.h"
+
+#include "imgui.h"
+#include "imgui_internal.h"
+
+namespace nimgui
+{
+	static void draw_vec3_widget(const std::string& label, glm::vec3& values, float columnWidth = 100.0f)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		auto boldFont = io.Fonts->Fonts[0];
+
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		ImGui::PushFont(boldFont);
+		ImGui::Button("X", buttonSize);
+		ImGui::PopFont();
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		ImGui::PushFont(boldFont);
+		ImGui::Button("Y", buttonSize);
+		ImGui::PopFont();
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		ImGui::PushFont(boldFont);
+		ImGui::Button("Z", buttonSize);
+		ImGui::PopFont();
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+
+		ImGui::PopStyleVar();
+
+
+	}
+}
+
