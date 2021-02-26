6eec724219a384d16ae9e9baa7785f127dd52987
jayanam <jayanam.games@gmail.com>
2021-02-26 21:00:45 +0100

    Init light settings

--- JGL_MeshLoader/source/elems/light.h
+++ JGL_MeshLoader/source/elems/light.h
@@ -13,7 +13,8 @@ namespace nelems
 
     Light()
     {
-      mColor = glm::vec3(1.0, 1.0f, 1.0f);
+      mColor = glm::vec3(1.0f, 1.0f, 1.0f);
+      mPosition = { 1.5f, 3.5f, 3.0f };
       mStrength = 100.0f;
     }
 
@@ -27,11 +28,11 @@ namespace nelems
       
     }
 
-    glm::vec3 mPosition = { 1.5f, 3.5f, 3.0f };
+    glm::vec3 mPosition;
 
-    glm::vec3 mColor = { 1.0f, 1.0f, 1.0f };
+    glm::vec3 mColor;
 
-    float mStrength = 100.0f;
+    float mStrength;
 
   };
 }
